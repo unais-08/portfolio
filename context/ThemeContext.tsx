43bffd5330c261132eae99796b696d73659205fe
Shaikh Unais <121447577+unais-08@users.noreply.github.com>
2025-08-28 22:27:39 +0530

    Update ThemeProvider to default to 'dark' theme

--- context/ThemeContext.tsx
+++ context/ThemeContext.tsx
@@ -18,30 +18,24 @@ const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 export const ThemeProvider: React.FC<{ children: ReactNode }> = ({
   children,
 }) => {
-  // Initialize theme from localStorage or default to 'light'
+  // Default is 'dark', unless user explicitly set 'light'
   const [theme, setTheme] = useState<"light" | "dark">(() => {
     if (typeof window !== "undefined") {
       const storedTheme = localStorage.getItem("theme");
-      if (
-        storedTheme === "dark" ||
-        (storedTheme === null &&
-          window.matchMedia("(prefers-color-scheme: dark)").matches)
-      ) {
-        return "dark";
+      if (storedTheme === "light") {
+        return "light";
       }
     }
-    return "light";
+    return "dark"; // default
   });
 
   useEffect(() => {
-    // Apply the 'dark' class to the html element based on the theme state
     const html = document.documentElement;
     if (theme === "dark") {
       html.classList.add("dark");
     } else {
       html.classList.remove("dark");
     }
-    // Store the current theme in localStorage
     localStorage.setItem("theme", theme);
   }, [theme]);
 
